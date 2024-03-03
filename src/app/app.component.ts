88119766163c9ded31b406b8a9eefd98d68312e8
DLANDALLY <landally@icloud.com>
2024-03-03 19:04:04 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./component/header/header.component";
-import { SidenavComponent } from "./component/sidenav/sidenav.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [ HeaderComponent, SidenavComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'handlerFront';
