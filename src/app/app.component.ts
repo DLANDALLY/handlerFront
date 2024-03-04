464af1e5499c371a0eaeac78c129f3e97ab3628f
DLANDALLY <landally@icloud.com>
2024-03-04 19:10:45 +0100

    creation toolbar not finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from "./component/article/article.component";
+import { SidnavComponent } from './component/sidnav/sidnav.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, ArticleComponent]
+    imports: [RouterOutlet, ArticleComponent, SidnavComponent]
 })
 export class AppComponent {
   title = 'handlerFront';
