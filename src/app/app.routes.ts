e3e13871c94856e6049daa3e51b6e9b71875391e
DLANDALLY <landally@icloud.com>
2024-03-04 16:46:47 +0100

    add a routing for api Article and install angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { ArticleComponent } from './component/article/article.component';
 
 export const routes: Routes = [
-    { path: 'article', component: ArticleComponent },
+    { path: '', component: ArticleComponent },
 ];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
   apiRootUri: 'http://api.streamer.com/api/v1/',
   storage: {
-    auth: {
-      strategy: 'session',
-      key: 'auth'
-    }
+    // auth: {
+    //   strategy: 'session',
+    //   key: 'auth'
+    // }
   }
 };
